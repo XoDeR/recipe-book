a00743f03dbecbd17cd9a3922c02ac9f26f5b154
Volodymyr <XoDeR@users.noreply.github.com>
2023-11-06 18:14:39 +0100

    added pages to recipes search

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState, useRef } from "react";
 import { searchRecipes } from "./Api";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
@@ -9,12 +9,26 @@ const App = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [recipes, setRecipes] = useState<Array<any>>([]);
 
+  const pageNumber = useRef(1);
+
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
 
     try {
       const { results } = await searchRecipes(searchTerm, 1);
       setRecipes(results);
+      pageNumber.current = 1;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleViewMoreClick = async () => {
+    try {
+      const nextPage = pageNumber.current + 1;
+      const nextRecipes = await searchRecipes(searchTerm, nextPage);
+      setRecipes((prevRecipes) => [...prevRecipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
     } catch (error) {
       console.error(error);
     }
@@ -35,6 +49,9 @@ const App = () => {
       {recipes.map((recipe: Recipe) => (
         <RecipeCard key={recipe.id} recipe={recipe} />
       ))}
+      <button className="view-more-button" onClick={handleViewMoreClick}>
+        View More
+      </button>
     </div>
   );
 };
